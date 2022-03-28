45b6aade4fa1a710d2cb193282691d320d513ca0
wangbo <781278657@qq.com>
2022-03-28 20:25:10 +0800

    update：初步完成 List 组件

--- src/components/list/src/list.vue
+++ src/components/list/src/list.vue
@@ -1,83 +1,107 @@
 <template>
-    <div class="bd-list">
-        <table>
-            <thead>
-            <tr>
-                <th v-if="rowSelection">
-                    <bd-checkbox v-model="selectedAll" @change="selectAllChange"></bd-checkbox>
-                </th>
-                <th v-for="(col, index) of columns" :key="index">
-                    {{ col.title }}
-                </th>
-            </tr>
-            </thead>
-            <tbody>
-            <tr
-                v-for="row of data"
-                :key="row.key"
-                @mouseenter="enterHandler"
-            >
-                <td v-if="rowSelection">
-                    <bd-checkbox v-model="row.selected"></bd-checkbox>
-                </td>
-                <td v-for="(col, index) of columns" :key="index">
-                    {{ row[col.dataIndex] }}
-                </td>
-            </tr>
-            </tbody>
-        </table>
-        <div class="list-footer">
-            <slot name="footer">
-            </slot>
-        </div>
+  <div class="bd-list">
+    <table>
+      <thead>
+      <tr>
+        <th v-if="rowSelection">
+          <bd-checkbox v-model="selectedAll" @change="selectAllChange"></bd-checkbox>
+        </th>
+        <th v-for="(col, index) of columns" :key="index">
+          {{ col.title }}
+        </th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr
+          v-for="row of data"
+          :key="row.key"
+          @mouseenter="enterHandler"
+          @mouseleave="leaveHandler"
+      >
+        <td v-if="rowSelection">
+          <bd-checkbox v-model="row.selected"></bd-checkbox>
+        </td>
+        <td v-for="(col, index) of columns" :key="index">
+          {{ row[col.dataIndex] }}
+        </td>
+      </tr>
+      </tbody>
+    </table>
+    <div class="list-footer">
+      <slot name="footer">
+      </slot>
     </div>
+  </div>
 
-    <teleport to="body">
-<!--        <div class="tr-focus-mask"-->
-<!--             style="position: fixed; width: 100%; height: 100%; top: 0; left: 0;  z-index: 1; background: rgba(0,0,0,.5)"-->
-<!--        >-->
-<!--        </div>-->
-    </teleport>
+  <teleport to="body">
+    <transition name="slide-fade">
+      <div v-if="showFocusMask" class="tr-focus-mask"/>
+    </transition>
+  </teleport>
 </template>
 
 <script lang="ts">
-import {defineComponent, toRefs, PropType} from "vue"
+import {defineComponent, toRefs, ref, PropType} from "vue"
 import type {TableData, Columns, RowSelection} from "./interface"
 import BdCheckbox from "@/components/checkbox/src/checkbox.vue"
 import {useListSelected} from "@/components/list/src/useListSelected";
 
 export default defineComponent({
-    name: "BdList",
-    components: {BdCheckbox},
-    props: {
-        data: {
-            type: Array as PropType<TableData>,
-            required: true
-        },
-        columns: {
-            type: Array as PropType<Columns>,
-            required: true
-        },
-        rowSelection: {
-            type: Object as PropType<RowSelection>,
-            default: () => ({})
-        }
+  name: "BdList",
+  components: {BdCheckbox},
+  props: {
+    data: {
+      type: Array as PropType<TableData>,
+      required: true
     },
-    emits: ['selectAllChange'],
-    setup(props, {emit}) {
-        const {data, columns, rowSelection} = toRefs(props)
-        const {selectedAll, selectAllChange} = useListSelected(data.value, rowSelection.value, emit)
-
-        function enterHandler(e: MouseEvent) {
-            // console.log(e.target);
-        }
-
-        return {
-            selectedAll,
-            selectAllChange,
-            enterHandler
-        }
+    columns: {
+      type: Array as PropType<Columns>,
+      required: true
+    },
+    rowSelection: {
+      type: Object as PropType<RowSelection>,
+      default: () => ({})
     }
+  },
+  emits: ['selectAllChange'],
+  setup(props, {emit}) {
+    const {data, columns, rowSelection} = toRefs(props)
+    const {selectedAll, selectAllChange} = useListSelected(data.value, rowSelection.value, emit)
+
+    const focusTime = ref(0)
+    const showFocusMask = ref(false)
+    let timer: number | null = null
+    const FOCUS_TIME = 2
+
+    function enterHandler(e: MouseEvent) {
+      timer = window.setInterval(() => {
+        focusTime.value++
+        console.log(focusTime.value);
+        if (focusTime.value === FOCUS_TIME) {
+          clearInterval(timer as number);
+          (e.target as HTMLTableRowElement).classList.add('focused')
+          showFocusMask.value = true
+          console.log(focusTime.value, '添加 class');
+        }
+      }, 1000)
+    }
+
+    function leaveHandler(e: MouseEvent) {
+      clearInterval(timer as number);
+      (e.target as HTMLTableRowElement).classList.remove('focused')
+      showFocusMask.value = false
+      focusTime.value = 0
+      console.log(focusTime.value);
+    }
+
+    return {
+      showFocusMask,
+      selectedAll,
+      selectAllChange,
+      enterHandler,
+      leaveHandler
+    }
+  }
 })
 </script>
 
