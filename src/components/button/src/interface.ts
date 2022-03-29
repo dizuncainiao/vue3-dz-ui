3d71d5e2553b56132dfce347ed0e49fae5f486d4
wangbo <781278657@qq.com>
2022-03-29 09:31:25 +0800

    init：优化

--- src/components/button/src/interface.ts
+++ src/components/button/src/interface.ts
@@ -2,12 +2,12 @@ export type ButtonSize = 'small' | 'default'
 
 export type ButtonType = 'default' | 'primary'
 
-export type IconType = 'icon-jiahao' | 'icon-sousuo'
+export type IconType = 'icon-jiahao' | 'icon-sousuo' | 'icon-icon-test1' | 'icon-jiazai_shuaxin'
 
 export interface Props {
-  type: ButtonType,
-  disabled: boolean,
-  round: boolean,
-  icon: IconType,
-  size: ButtonSize
+    type: ButtonType,
+    disabled: boolean,
+    round: boolean,
+    icon: IconType,
+    size: ButtonSize
 }
--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import VueTypeImports from 'vite-plugin-vue-type-imports'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/ts-vue3-components/',
   plugins: [
     vue(),
     DefineOptions(),
