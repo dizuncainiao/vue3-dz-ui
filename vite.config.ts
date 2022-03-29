f4e91157b6d77b2156053809018a1154ca701d42
wangbo <781278657@qq.com>
2022-03-29 09:33:49 +0800

    update：生产环境配置

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import VueTypeImports from 'vite-plugin-vue-type-imports'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/ts-vue3-components/',
+  base: process.env.NODE_ENV === 'production' ? '/ts-vue3-components/' : '',
   plugins: [
     vue(),
     DefineOptions(),
