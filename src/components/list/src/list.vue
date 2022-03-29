bb69b6b7964c8faa5a0679ead9e008955c433aa1
wangbo <781278657@qq.com>
2022-03-29 08:52:05 +0800

    update：逻辑解耦

--- src/components/list/src/list.vue
+++ src/components/list/src/list.vue
@@ -41,10 +41,12 @@
 </template>
 
 <script lang="ts">
-import {defineComponent, toRefs, ref, PropType} from "vue"
+import {defineComponent, toRefs, PropType} from "vue"
 import type {TableData, Columns, RowSelection} from "./interface"
 import BdCheckbox from "@/components/checkbox/src/checkbox.vue"
-import {useListSelected} from "@/components/list/src/useListSelected";
+import {useListSelected} from "./useListSelected"
+import {showFocusMask, enterHandler, leaveHandler} from './useTableRowFocus'
+
 
 export default defineComponent({
   name: "BdList",
@@ -65,39 +67,13 @@ export default defineComponent({
   },
   emits: ['selectAllChange'],
   setup(props, {emit}) {
-    const {data, columns, rowSelection} = toRefs(props)
+    const {data, rowSelection} = toRefs(props)
     const {selectedAll, selectAllChange} = useListSelected(data.value, rowSelection.value, emit)
 
-    const focusTime = ref(0)
-    const showFocusMask = ref(false)
-    let timer: number | null = null
-    const FOCUS_TIME = 2
-
-    function enterHandler(e: MouseEvent) {
-      timer = window.setInterval(() => {
-        focusTime.value++
-        console.log(focusTime.value);
-        if (focusTime.value === FOCUS_TIME) {
-          clearInterval(timer as number);
-          (e.target as HTMLTableRowElement).classList.add('focused')
-          showFocusMask.value = true
-          console.log(focusTime.value, '添加 class');
-        }
-      }, 1000)
-    }
-
-    function leaveHandler(e: MouseEvent) {
-      clearInterval(timer as number);
-      (e.target as HTMLTableRowElement).classList.remove('focused')
-      showFocusMask.value = false
-      focusTime.value = 0
-      console.log(focusTime.value);
-    }
-
     return {
-      showFocusMask,
       selectedAll,
       selectAllChange,
+      showFocusMask,
       enterHandler,
       leaveHandler
     }
