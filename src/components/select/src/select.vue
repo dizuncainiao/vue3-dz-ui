2d982677a69e13420327d6fc1f4493c01bb85f89
wangbo <781278657@qq.com>
2022-03-30 19:19:26 +0800

    add：初始 select 组件

---
+++ src/components/select/src/select.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="dz-select">
+
+  </div>
+</template>
+
+<script lang="ts">
+import {defineComponent, toRefs, PropType, computed} from "vue"
+
+export default defineComponent({
+  name: "DzCheckbox",
+  props: {
+    modelValue: {
+      type: Boolean as PropType<boolean>,
+      default: false
+    },
+    disabled: {
+      type: Boolean as PropType<boolean>,
+      default: false
+    }
+  },
+  emits: ['update:modelValue', 'change'],
+  setup(props, {emit}) {
+  }
+})
+</script>
+
+<style scoped lang="less">
+@import "../style/select";
+</style>
