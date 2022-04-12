6d16773d9f6ac390b64a4530676e2ea2e7934519
wangbo <781278657@qq.com>
2022-04-12 14:17:41 +0800

    fix：issues #2

--- src/components/list/src/list.vue
+++ src/components/list/src/list.vue
@@ -2,57 +2,57 @@
   <div class="dz-list">
     <table>
       <thead>
-        <tr>
-          <th v-if="rowSelection">
-            <dz-checkbox
+      <tr>
+        <th v-if="rowSelection">
+          <dz-checkbox
               v-model="selectedAll"
               @change="selectAllChange"
-            ></dz-checkbox>
-          </th>
-          <th v-for="(col, index) of columns" :key="index">
-            {{ col.title }}
-          </th>
-        </tr>
+          ></dz-checkbox>
+        </th>
+        <th v-for="(col, index) of columns" :key="index">
+          {{ col.title }}
+        </th>
+      </tr>
       </thead>
       <tbody>
-        <tr
+      <tr
           v-for="row of data"
           :key="row.key"
           @mouseenter="enterHandler"
           @mouseleave="leaveHandler"
-        >
-          <td v-if="rowSelection">
-            <dz-checkbox v-model="row.selected"></dz-checkbox>
-          </td>
-          <td v-for="(col, index) of columns" :key="index">
-            {{ row[col.dataIndex] }}
-          </td>
-        </tr>
+      >
+        <td v-if="rowSelection">
+          <dz-checkbox v-model="row.selected"></dz-checkbox>
+        </td>
+        <td v-for="(col, index) of columns" :key="index">
+          {{ row[col.dataIndex] }}
+        </td>
+      </tr>
       </tbody>
     </table>
     <div class="list-footer">
-      <slot name="footer"> </slot>
+      <slot name="footer"></slot>
     </div>
   </div>
 
   <teleport to="body">
     <transition name="slide-fade">
-      <div v-if="showFocusMask" class="tr-focus-mask" />
+      <div v-if="showFocusMask" class="tr-focus-mask"/>
     </transition>
   </teleport>
 </template>
 
 <script lang="ts">
-import { defineComponent, toRefs } from 'vue'
-import { useListSelected } from './useListSelected'
-import { showFocusMask, enterHandler, leaveHandler } from './useTableRowFocus'
-import type { PropType } from 'vue'
-import type { TableData, Columns, RowSelection } from './interface'
+import {defineComponent, toRefs, onBeforeUnmount} from 'vue'
+import {useListSelected} from './useListSelected'
+import {showFocusMask, enterHandler, leaveHandler} from './useTableRowFocus'
+import type {PropType} from 'vue'
+import type {TableData, Columns, RowSelection} from './interface'
 import DzCheckbox from '@/components/checkbox/src/checkbox.vue'
 
 export default defineComponent({
   name: 'DzList',
-  components: { DzCheckbox },
+  components: {DzCheckbox},
   props: {
     data: {
       type: Array as PropType<TableData>,
@@ -68,13 +68,17 @@ export default defineComponent({
     },
   },
   emits: ['selectAllChange'],
-  setup(props, { emit }) {
-    const { data, rowSelection } = toRefs(props)
-    const { selectedAll, selectAllChange } = useListSelected(
-      data.value,
-      rowSelection.value,
-      emit
+  setup(props, {emit}) {
+    const {data, rowSelection} = toRefs(props)
+    const {selectedAll, selectAllChange} = useListSelected(
+        data.value,
+        rowSelection.value,
+        emit
     )
+
+    onBeforeUnmount(() => {
+      showFocusMask.value = false
+    })
 
     return {
       selectedAll,
