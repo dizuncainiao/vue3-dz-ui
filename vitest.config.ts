5a482b3facbe23e757270ef71812b70d39fc6f8b
wangbo <781278657@qq.com>
2022-05-25 21:52:01 +0800

    style: 💄 添加忽略

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,6 +5,8 @@ import Vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [Vue()],
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
   test: {
     globals: true,
     environment: 'jsdom',
